7150cb6eb7d3a33fc04f310e1c7870f884675ae2
Keeghan McGarry <keeghan_m@live.com>
2025-06-01 23:06:14 +0100

    compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import node from '@astrojs/node'
 import partytown from '@astrojs/partytown'
 import tailwindcss from '@tailwindcss/vite'
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [react(), partytown(), compress()],
   output: 'static',
   adapter: node({ mode: 'standalone' }),
   vite: { plugins: [tailwindcss()] },
